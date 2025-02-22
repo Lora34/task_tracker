9b0b3b09695ed3be33dfe6932c3359d7f1fa0c13
Lora34 <my34pochta@gmail.com>
2025-02-22 18:55:43 +0300

    feature: add cors

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from database import create_tables, delete_tables
-
 from router import router as tasks_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -18,3 +17,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(tasks_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
